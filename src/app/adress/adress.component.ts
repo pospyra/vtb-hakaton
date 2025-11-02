fc085063f428064283ed6acb8605c9035a4926aa
К Б <yabkn@outlook.com>
2025-11-02 12:10:10 +0700

    компонент адрес

---
+++ src/app/adress/adress.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adress',
+  standalone: true,
+  imports: [],
+  templateUrl: './adress.component.html',
+  styleUrl: './adress.component.css'
+})
+export class AdressComponent {
+
+}
