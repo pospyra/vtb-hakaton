8909a20f936568cb55b6ece4e9cdfceed5c0eb43
К Б <yabkn@outlook.com>
2025-11-02 13:03:50 +0700

    добавление алгоритма работы

--- src/app/adress/adress.component.ts
+++ src/app/adress/adress.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-adress',
@@ -9,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class AdressComponent {
 
+  //constructor(private dataService: DataService,
+  constructor(
+    private router: Router) { }
+
+  sendAdress() {
+    this.router.navigate(["/"]);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AdressComponent } from "./adress/adress.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "adress",
+    pathMatch: "full"
+  },
+  { path: "adress", component: AdressComponent },
+  //{ path: "devices", component: DeviceListComponent },
+  //{ path: "products/edit/:id", component: ProductCreateEditComponent }
+];
